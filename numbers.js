34ebfb723953f93224723a37be836cdd28da22bd
Emma Johnson <emma.j@cox.net>
2019-08-12 20:32:15 -0500

    help pls

---
+++ numbers.js
@@ -0,0 +1,65 @@
+const greisch = {
+    w1 : "0",
+    w2 : "0",
+    w3 : "0",
+    w4 : "0",
+};
+
+const golka = {
+    w1 : "0",
+    w2 : "0",
+    w3 : "0",
+    w4 : "0",
+};
+
+const spanny = {
+    w1 : "0",
+    w2 : "0",
+    w3 : "0",
+    w4 : "0",
+};
+
+const delkamiller = {
+    w1 : "0",
+    w2 : "0",
+    w3 : "0",
+    w4 : "0",
+};
+
+const cotton = {
+    w1 : "0",
+    w2 : "0",
+    w3 : "0",
+    w4 : "0",
+};
+
+const bauman = {
+    w1 : "0",
+    w2 : "0",
+    w3 : "0",
+    w4 : "0",
+};
+
+const piernicky = {
+    w1 : "0",
+    w2 : "0",
+    w3 : "0",
+    w4 : "0",
+};
+
+const sisson = {
+    w1 : "0",
+    w2 : "0",
+    w3 : "0",
+    w4 : "0",
+};
+
+const mcgill = {
+    w1 : "0",
+    w2 : "0",
+    w3 : "0",
+    w4 : "0",
+};
+
+
+export { greisch , golka , spanny , delkamiller , cotton , bauman , piernicky , sisson , mcgill };
