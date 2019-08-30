a726d9f0c4a9a06860dccccfdc42ba63594496fa
Emma Johnson <emma.j@cox.net>
2019-08-30 10:33:43 -0500

    first number set

--- numbers.js
+++ numbers.js
@@ -1,20 +1,20 @@
-const greisch = [0,0,0,0];
+const greisch = [140.00,0,0,0];
 
-const golka = [0,0,0,0];
+const golka = [258.00,0,0,0];
 
-const spanny = [0,0,0,0];
+const spanny = [830.00,0,0,0];
 
-const delkamiller = [0,0,0,0];
+const delkamiller = [32.00,0,0,0];
 
-const cotton = [0,0,0,0];
+const cotton = [392.00,0,0,0];
 
-const bauman = [0,0,0,0];
+const bauman = [387.00,0,0,0];
 
-const piernicky = [0,0,0,0];
+const piernicky = [525.00,0,0,0];
 
-const sisson = [0,0,0,0];
+const sisson = [292.00,0,0,0];
 
-const mcGill = [0,0,0,0];
+const mcGill = [113.41,0,0,0];
 
 
 export { greisch , golka , spanny , delkamiller , cotton , bauman , piernicky , sisson , mcGill };
