ebf96899b708e1822c0ebf2f209c636c0494c397
Emma Johnson <emma.j@cox.net>
2019-09-05 07:14:42 -0500

    new numbers

--- numbers.js
+++ numbers.js
@@ -1,16 +1,16 @@
 const greisch = [140.00,0,0,0];
 
-const golka = [258.00,0,0,0];
+const golka = [378.00,0,0,0];
 
 const spanny = [830.00,0,0,0];
 
-const delkamiller = [32.00,0,0,0];
+const delkamiller = [77.05,0,0,0];
 
 const cotton = [392.00,0,0,0];
 
-const bauman = [387.00,0,0,0];
+const bauman = [537.00,0,0,0];
 
-const piernicky = [525.00,0,0,0];
+const piernicky = [625.00,0,0,0];
 
 const sisson = [292.00,0,0,0];
 
