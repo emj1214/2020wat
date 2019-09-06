69a752a4cf7b8bc4b1a43bfd5ac6b737389f1ad9
Emma Johnson <emma.j@cox.net>
2019-09-06 13:51:33 -0500

    week 2 numbers

--- numbers.js
+++ numbers.js
@@ -1,20 +1,20 @@
-const greisch = [140.00,0,0,0];
+const greisch = [140.00, 640.00, 0, 0];
 
-const golka = [378.00,0,0,0];
+const golka = [378.00, 240.00, 0, 0];
 
-const spanny = [830.00,0,0,0];
+const spanny = [830.00, 151.00, 0, 0];
 
-const delkamiller = [77.05,0,0,0];
+const delkamiller = [77.05, 495.00, 0, 0];
 
-const cotton = [392.00,0,0,0];
+const cotton = [392.00, 57.00, 0, 0];
 
-const bauman = [537.00,0,0,0];
+const bauman = [537.00, 350.00, 0, 0];
 
-const piernicky = [625.00,0,0,0];
+const piernicky = [625.00, 459.10, 0, 0];
 
-const sisson = [292.00,0,0,0];
+const sisson = [292.00, 236.00, 0, 0];
 
-const mcGill = [113.41,0,0,0];
+const mcGill = [113.41, 235.00, 0, 0];
 
 
 export { greisch , golka , spanny , delkamiller , cotton , bauman , piernicky , sisson , mcGill };
