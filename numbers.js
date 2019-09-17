718ecf27c7278c10e0c61ee2f0879e7fb185df5b
Emma Johnson <emma.j@cox.net>
2019-09-17 13:30:34 -0500

    new numbers

--- numbers.js
+++ numbers.js
@@ -1,20 +1,20 @@
-const greisch = [140.00, 640.01, 121.00, 0];
+const greisch = [140.00, 640.01, 196.00, 0];
 
 const golka = [378.00, 240.19, 773.00, 0];
 
-const spanny = [830.00, 151.00, 120.00, 0];
+const spanny = [830.00, 151.00, 460.00, 0];
 
-const delkamiller = [77.05, 550.00, 540.00, 0];
+const delkamiller = [77.05, 550.00, 785.00, 0];
 
-const cotton = [392.00, 4057.00, 265.00, 0];
+const cotton = [392.00, 4057.00, 415.00, 0];
 
-const bauman = [537.00, 500.00, 440.00, 0];
+const bauman = [537.00, 500.00, 655.00, 0];
 
-const piernicky = [625.00, 458.60, 147.00, 0];
+const piernicky = [625.00, 458.60, 287.00, 0];
 
 const sisson = [292.00, 236.00, 130.00, 0];
 
-const mcGill = [113.41, 235.00, 1340.59, 0];
+const mcGill = [113.41, 235.00, 1341, 0];
 
 
 export { greisch , golka , spanny , delkamiller , cotton , bauman , piernicky , sisson , mcGill };
